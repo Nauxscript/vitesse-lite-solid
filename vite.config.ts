a63ed8a5f19e534fdc3e4ee91fc7448161ce6efa
nauxscript <nauxscript@gmail.com>
2023-02-02 17:52:38 +0800

    feat: setup auto-imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import Solid from 'solid-start/vite'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-  plugins: [Solid(), UnoCSS()],
+  plugins: [
+    Solid(),
+    UnoCSS(),
+    AutoImport({
+      imports: ['solid-js', 'solid-app-router'],
+      dts: './src/auto-imports.d.ts',
+      // resolvers: [
+      //   IconsResolver({
+      //     componentPrefix: 'Icon',
+      //   }),
+      // ],
+    }),
+  ],
 })
