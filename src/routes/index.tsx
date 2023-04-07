38879f8a504ba16163c96301a92aace47bbac6da
nauxscript <nauxscript@gmail.com>
2023-04-07 11:46:18 +0800

    chore: home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,21 @@
 export default function Home() {
   return (
-    <main>
-      <h1 text-red class="bg-gray">Hello solid!</h1>
-      <p text-green>have HMR？</p>
-      <p>yes!</p>
-      <div class="i-carbon-list-boxes" />
+    <main px-4 py-10 text-center text-gray-700 dark:text-gray-200>
+      <div text-4xl>
+        <div i-carbon-mountain inline-block=""></div>
+      </div>
+      <em text-sm opacity-75>Opinionated Vite Starter Template for <a href="" text-gray-700 hover:text-dark decoration-none><strong>Solid</strong></a></em>
+      <nav text-xl mt-6 >
+        <a href="/" mx-2>
+          <div i-carbon-mountain inline-block="" text-gray hover:text-dark></div>
+        </a>
+        <a href="/about" mx-2>
+          <div i-carbon-content-view inline-block="" text-gray hover:text-dark></div>
+        </a>
+        <a href="https://github.com/Nauxscript/vitesse-lite-solid" mx-2>
+          <div i-carbon-logo-github inline-block="" text-gray hover:text-dark></div>
+        </a>
+      </nav>
     </main>
   )
 }
