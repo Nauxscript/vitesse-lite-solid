3ad496aa9776d9927d6cee0b6e66269fed566635
nauxscript <nauxscript@gmail.com>
2023-04-07 14:04:56 +0800

    fix: updated link

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,10 +1,11 @@
+import { A } from 'solid-start'
 export default function About() {
   return (
     <main px-4 py-10 text-center text-gray-500>
       <div i-carbon-content-view inline-block="" text-gray text-4xl></div>
       <div text-gray text-2xl> About </div>
       <p>a easy starter template for Solid, setup by <a href="https://github.com/Nauxscript">me</a>, inspired by <a href="https://github.com/antfu">@Anthony Fu</a></p>
-      <a href="/">../</a>
+      <A href="/">../</A>
     </main>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { A } from 'solid-start'
 export default function Home() {
   return (
     <main px-4 py-10 text-center>
@@ -6,15 +7,15 @@ export default function Home() {
       </div>
       <em text-sm opacity-75>Opinionated Vite Starter Template for <a href="" text-gray-700 hover:text-dark decoration-none><strong>Solid</strong></a></em>
       <nav text-xl mt-6 >
-        <a href="/" mx-2>
-          <div i-carbon-mountain inline-block="" text-gray hover:text-dark></div>
-        </a>
-        <a href="/about" mx-2>
-          <div i-carbon-content-view inline-block="" text-gray hover:text-dark></div>
-        </a>
-        <a href="https://github.com/Nauxscript/vitesse-lite-solid" mx-2>
-          <div i-carbon-logo-github inline-block="" text-gray hover:text-dark></div>
-        </a>
+        <A href="/">
+          <div i-carbon-mountain inline-block="" text-gray hover:text-dark mx-1></div>
+        </A>
+        <A href="/about">
+          <div i-carbon-content-view inline-block="" text-gray hover:text-dark mx-1></div>
+        </A>
+        <A href="https://github.com/Nauxscript/vitesse-lite-solid">
+          <div i-carbon-logo-github inline-block="" text-gray hover:text-dark mx-1></div>
+        </A>
       </nav>
     </main>
   )
