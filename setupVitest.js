b2dd386ed6f313bdf9203b6bad7cb8256f84ad38
nauxscript <nauxscript@gmail.com>
2023-09-02 10:34:31 +0800

    chore: code lint

--- setupVitest.js
+++ setupVitest.js
@@ -1,5 +1,7 @@
 import '@testing-library/jest-dom'
+
 // refer to https://github.com/testing-library/jest-dom/issues/439
 import { expect } from 'vitest'
 import * as matchers from '@testing-library/jest-dom/matchers'
+
 expect.extend(matchers)
