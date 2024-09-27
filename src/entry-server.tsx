c4b0af4b9a890342d8191ff4421cea1eafdb33d8
nauxscript <nauxscript@gmail.com>
2024-09-27 11:40:22 +0800

    fix: ico

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,7 +9,7 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="Opinionated Vite Starter Template" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.svg" />
           {assets}
         </head>
         <body>
